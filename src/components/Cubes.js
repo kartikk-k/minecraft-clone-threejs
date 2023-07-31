2f11de2c99d26ed7ae88fd3ca83017278bb183f4
Kartik Khorwal <hellokartikk@gmail.com>
2023-07-31 17:40:58 +0530

    add: readme

--- src/components/Cubes.js
+++ src/components/Cubes.js
@@ -23,7 +23,7 @@ const Cubes = () => {
 
     }, [])
 
-
+    console.log(cubes)
     return cubes.map(({ key, position, texture }) => (
         <Cube key={key} position={position} texture={texture} />
     ))
